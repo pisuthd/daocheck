8d53ef88b02e274c88baaa679ed835c70a56a412
pisuthd <pisuth.dae@gmail.com>
2023-11-05 20:21:43 +0900

    [contracts] init hardhat project / add circom lib

---
+++ packages/contracts/hardhat.config.js
@@ -0,0 +1,28 @@
+require("@nomicfoundation/hardhat-toolbox");
+
+require("dotenv").config()
+
+const PRIVATE_KEY = process.env.PRIVATE_KEY
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  mocha: {
+    timeout: 1200000,
+  },
+  solidity: {
+    version: "0.8.17",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    },
+  },
+  networks: {
+    zkatana: {
+      chainId: 1261120,
+      url: "https://rpc.zkatana.gelato.digital",
+      accounts: [PRIVATE_KEY],
+    }
+  }
+};
